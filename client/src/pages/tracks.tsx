be663811bc116fdd0d00a29a9ba81d4b10261904
Álvaro Marrupe Rodríguez <alvaromarrupe@Mini-de-Alvaro.lan>
2025-04-05 14:16:10 +0200

    The useQuery Hook

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Layout } from "../components";
-import { gql } from "../__generated__/";
+import { Layout, QueryResult } from "../components";
+import { gql } from "../__generated__";
+import { useQuery } from "@apollo/client";
+import TrackCard from "../containers/track-card";
 
 const TRACKS = gql(`
   query GetTracks {
@@ -24,7 +26,17 @@ const TRACKS = gql(`
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
